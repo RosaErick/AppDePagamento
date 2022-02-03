bd6ccb1ee42d2cb5b38271e29a894e5034369f82
Erick Rosa <erickpmotta@gmail.com>
2022-02-03 02:34:11 -0300

    starting project, fetching items from api and rendering on screen

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,1 +1,47 @@
 import react from "react";
+import { getUser } from "./getUser";
+import { useState, useEffect } from "react";
+import { Component } from "react/cjs/react.production.min";
+
+const API_URL = "https://www.mocky.io/v2/5d531c4f2e0000620081ddce";
+
+class UserList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.fetchUserList();
+  }
+
+  async fetchUserList() {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      this.setState({ users: data });
+
+      console.log(this.state);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  createList() {
+    return this.state.users.map((item) => {
+      return (
+        <>
+          <h1>{item.name}</h1>
+        </>
+      );
+    });
+  }
+
+  render() {
+    return <li>{this.createList()}</li>;
+  }
+}
+
+export default UserList;
--- src/components/getUser.jsx
+++ src/components/getUser.jsx
@@ -1,3 +1,16 @@
+import react, { Component }  from "react";
+
+const API_URL = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce';
 
 
-const API_URL = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce'
+const getUser = async () => {
+const response = await fetch(API_URL);
+const data = await response.json();
+     
+
+ console.log(data)
+    return ([data]);
+}
+  
+
+export { getUser }
