4016d35830f0404eda306a3f49a2cefdb8aec8ab
Erick Rosa <erickpmotta@gmail.com>
2022-02-05 23:44:27 -0300

    start styling components

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,6 +2,37 @@ import react from "react";
 import { getUser } from "./getUser";
 import { useState, useEffect } from "react";
 import { Component } from "react";
+import styled from "styled-components";
+
+const UserBox = styled.ul`
+  padding: 0;
+  margin: 0 0 2px 0;
+
+  li {
+    display: flex;
+    background-color: #393a5e;
+    background: linear-gradient(180deg, #393a5e 0%, #282a3f 100%);
+    color: #fff;
+    justify-content: space-around;
+    align-items: center;
+  }
+  img {
+    padding: 20px;
+    border-radius: 150px;
+  }
+
+  .userData {
+    flex: 1;
+  }
+
+  .username {
+    margin: 0;
+  }
+
+  button {
+    margin: 20px;
+  }
+`;
 
 const API_URL = "https://www.mocky.io/v2/5d531c4f2e0000620081ddce";
 
@@ -33,20 +64,21 @@ class UserList extends Component {
     return this.state.users.map((item) => {
       return (
         <>
-          <ul>
+          <UserBox>
             <li>
               <img className="userImg" src={item.img} alt="" />
               <div className="userData">
                 <p className="username">{item.name}</p>
-                <p className="userid">ID: {item.id}</p>
-                <p className="username">Username: {item.username}</p>
+                <p className="username">
+                  ID: {item.id} - Username: {item.username}
+                </p>
               </div>
 
               <div className="paybutton">
                 <button onCLick>Pagar</button>
               </div>
             </li>
-          </ul>
+          </UserBox>
         </>
       );
     });
